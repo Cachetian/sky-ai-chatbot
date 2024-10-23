fb3388007903acf9c0e5e41db10957477e15b04b
Sky Mu <sky.mu@sap.com>
2024-10-23 23:51:07 +0800

    Step 1 easy adapt to ollama model

--- ai/index.ts
+++ ai/index.ts
@@ -1,8 +1,13 @@
-import { openai } from "@ai-sdk/openai";
+import { openai, createOpenAI } from "@ai-sdk/openai";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
+const model = createOpenAI({
+  baseURL: 'http://127.0.0.1:11434/v1/',
+  apiKey: 'ollama'
+}).chat("llama3.1");
+
 export const customModel = wrapLanguageModel({
-  model: openai("gpt-4o"),
+  model: model,
   middleware: customMiddleware,
 });
