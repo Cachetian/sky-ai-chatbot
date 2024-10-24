054f6f8e2deb4dd0d850e7c0becac0c85c595119
Sky Mu <sky.mu@sap.com>
2024-10-24 16:04:46 +0800

    Step 2 refactor to process env

--- ai/index.ts
+++ ai/index.ts
@@ -2,10 +2,14 @@ import { openai, createOpenAI } from "@ai-sdk/openai";
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
+let modelName: string = "";
+if (process.env.Z_OPENAI_API_MODEL) {
+  modelName = process.env.Z_OPENAI_API_MODEL;
+}
 const model = createOpenAI({
-  baseURL: 'http://127.0.0.1:11434/v1/',
-  apiKey: 'ollama'
-}).chat("llama3.1");
+  baseURL: process.env.Z_OPENAI_API_URL,
+  apiKey: process.env.Z_OPENAI_API_KEY
+}).chat(modelName);
 
 export const customModel = wrapLanguageModel({
   model: model,
