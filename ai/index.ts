2a635336597aacd4a1c0b557328f4a509f97c577
Sky Mu <sky.mu@sap.com>
2024-11-06 23:01:06 +0800

    Step 2 fully replace ollama model

--- ai/index.ts
+++ ai/index.ts
@@ -1,4 +1,4 @@
-import { openai, createOpenAI } from "@ai-sdk/openai";
+import { createOllama } from 'ollama-ai-provider';
 import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
@@ -6,10 +6,10 @@ let modelName: string = "";
 if (process.env.Z_OPENAI_API_MODEL) {
   modelName = process.env.Z_OPENAI_API_MODEL;
 }
-const model = createOpenAI({
+
+const model = createOllama({
   baseURL: process.env.Z_OPENAI_API_URL,
-  apiKey: process.env.Z_OPENAI_API_KEY
-}).chat(modelName);
+})(modelName);
 
 export const customModel = wrapLanguageModel({
   model: model,
